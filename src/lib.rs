1b9b752165a451f45d8ba2c2ae65b138c796cca3
ki11errabbit <ki11errabbit@posteo.net>
2024-02-08 19:54:30 -0700

    added code for rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 use std::collections::HashMap;
 use std::process::Command;
-use std::os::unix::net::UnixStream;
+use std::os::unix::net::UnixListener;
+use std::io::prelude::*;
+
+use json::JsonValue;
 
 
-
+#[derive(Debug, PartialEq)]
 pub enum Value {
     Integer(i64),
     String(String),
     Float(f64),
-    Dictionary(HashMap<String, Value>),
+    Dictionary(Vec<(String, Value)>),
     List(Box<[Value]>),
+    Boolean(bool),
+    Null,
 }
 
 impl Value {
@@ -34,6 +39,9 @@ impl Value {
                 result.push_str("]");
                 result
             }
+            Value::Boolean(b) => b.to_string(),
+            Value::Null => "null".to_string(),
+                    
         }
     }
 
@@ -47,6 +55,48 @@ impl Value {
         result.push_str("]");
         return result;
     }
+    
+    pub fn from_json_value(value: JsonValue) -> Value {
+        match value {
+            JsonValue::Number(n) => {
+                let temp: f64 = n.into();
+                if temp % 1.0 != 0.0f64 {
+                    Value::Float(n.into())
+                } else {
+                    Value::Integer(n.as_fixed_point_i64(0).expect("Number was not an integer"))
+                }
+            }
+            JsonValue::String(s) => {
+                let int = s.parse::<i64>();
+                let float = s.parse::<f64>();
+                if let Ok(i) = int {
+                    Value::Integer(i)
+                } else if let Ok(f) = float {
+                    Value::Float(f)
+                } else {
+                    Value::String(s)
+                }
+            },
+            JsonValue::Object(o) => {
+                let mut map = Vec::new();
+                for (key, value) in o.iter() {
+                    map.push((key.to_string(), Value::from_json_value(value.clone())));
+                }
+                Value::Dictionary(map)
+            }
+            JsonValue::Array(a) => {
+                let mut list = Vec::new();
+                for value in a.iter() {
+                    list.push(Value::from_json_value(value.clone()));
+                }
+                Value::List(list.into_boxed_slice())  
+            }
+            JsonValue::Boolean(b) => Value::Boolean(b),
+            JsonValue::Null => Value::Null,
+            JsonValue::Short(_) => panic!(),
+        }
+            
+    }
 }
 
 
@@ -58,7 +108,7 @@ pub struct ForeignFunction<'a> {
 
 
 impl<'a> ForeignFunction<'a> {
-    pub fn new<S>(name: &'a S) -> ForeignFunction<'a> 
+    pub fn new<S: ?Sized>(name: &'a S) -> ForeignFunction<'a> 
     where S: AsRef<str> {
     Self {
             name: name.as_ref(),
@@ -78,9 +128,103 @@ impl ForeignFunction<'_> {
         let pid = std::process::id();
         let socket_path = format!("/tmp/uffi_{}", pid);
         command.env("UFFI_SOCKET", &socket_path);
-        let handle = command.spawn().unwrap();
+        let mut handle = command.spawn().unwrap();
 
-        let mut stream = UnixStream::connect(socket_path).unwrap();
+        let listener = UnixListener::bind(&socket_path).unwrap();
+        let (mut stream, _) = listener.accept().unwrap();
+        let mut json_string = String::new();
+        let mut buffer = [0; 1024];
+        loop {
+            let bytes = stream.read(&mut buffer).unwrap();
+            if bytes == 0 {
+                break;
+            }
+            json_string.push_str(&String::from_utf8_lossy(&buffer[..bytes]));
+        }
+        eprintln!("{}", json_string);
+        let json = ForeignFunction::read_json(json_string);
+        let _ = handle.wait();
 
+        stream.shutdown(std::net::Shutdown::Both).unwrap();
+        std::fs::remove_file(socket_path).unwrap();
+
+        Value::from_json_value(json)
+
+
+
+    }
+    
+
+    fn read_json(string: String) -> JsonValue {
+        return string.into()
+    }
+}
+
+pub struct Args {
+    args: Vec<Value>,
+}
+
+impl Args {
+    pub fn from_json(json: JsonValue) -> Args {
+        let mut args = Vec::new();
+        for value in json.members() {
+            args.push(Value::from_json_value(value.clone()));
+        }
+        Args { args }
+    }
+}
+
+
+impl Iterator for Args {
+    type Item = Value;
+    fn next(&mut self) -> Option<Value> {
+        self.args.drain(..1).next()
+    }
+}
+
+impl DoubleEndedIterator for Args {
+    fn next_back(&mut self) -> Option<Value> {
+        self.args.pop()
+    }
+
+    fn nth_back(&mut self, n: usize) -> Option<Value> {
+        let mut back = self.args.pop();
+        for _ in 0..(n -1) {
+            back = self.args.pop();
+        }
+        back
+    }
+}
+
+pub fn args() -> Args {
+    let args = std::env::var("UFFI_ARGS").unwrap();
+    let json = args.into();
+    Args::from_json(json)
+
+}
+
+
+#[macro_export]
+macro_rules! uffi_return {
+    ($e:expr) => {
+        let json = $e.into();
+        let socket_path = std::env::var("UFFI_SOCKET").unwrap();
+        let mut stream = std::os::unix::net::UnixStream::connect(&socket_path).unwrap();
+        stream.write_all(json.dump().as_bytes()).unwrap();
+        stream.shutdown(std::net::Shutdown::Both).unwrap();
+        std::process::exit(0);
+    };
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_foreign_function() {
+        let function = ForeignFunction::new("python");
+        let result = function.call(&[Value::String("python/test2.py".to_string())]);
+        assert_eq!(result, Value::Integer(77));
     }
 }
